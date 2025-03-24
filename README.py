0803d317653000e8134849eefde84ce6e549b378
yasserel1 <yasserelmazroub@gmail.com>
2025-03-24 12:56:18 +0100

    Update and rename README.py to README.txt

--- README.py
+++
@@ -1,8 +0,0 @@
-
-Here’s a more professional version of your text:
-
-My name is Yasser, and I am from Morocco. I am currently at an intermediate level in programming, having recently embarked on this exciting journey. As I strive to improve my skills and deepen my understanding of the field, I will be sharing updates on my progress, projects, and learning experiences here.
-
-I am eager to connect with and learn from experienced professionals and like-minded individuals in the programming community. Your guidance, feedback, and support will be invaluable in helping me grow and achieve my goals in this ever-evolving domain.
-
-Thank you in advance for being part of my development journey!
---
+++ README.txt
@@ -0,0 +1,4 @@
+💫 About Me:
+i am new frontend, backend développer
+i use HTML, CSS, JS, Python, PHP
+AND also i know something about cyber Security
